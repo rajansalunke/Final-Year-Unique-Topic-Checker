650e50979dc5b54bc06d8b5cdb7b92ac34ea5dee
Rajan Salunkhe <84559031+rajansalunke@users.noreply.github.com>
2024-04-07 11:54:14 +0530

    Add files via upload

---
+++ project_topic_repeatation/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for project_topic_repeatation project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_topic_repeatation.settings')
+
+application = get_asgi_application()
